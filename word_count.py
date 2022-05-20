b488fe6c7bc3266803881db0f66607f088312129
arstep-ds <arstep.ds@gmail.com>
2022-05-20 13:34:48 +0200

    some changes

--- word_count.py
+++ word_count.py
@@ -11,5 +11,6 @@ def count_words(filename):
         num_words = len(words)
         print(f"The file {filename} has about {num_words} words in it.")
 
-filename = "alice.txt"
-count_words(filename)
+filenames = ["alice.txt", "moby.txt", "sidd.txt"]
+for filename in filenames:
+    count_words(filename)
